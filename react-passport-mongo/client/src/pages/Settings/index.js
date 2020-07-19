dbcef408fc264e6ed78b8d73ae2f203f3d461c33
tango <sm91wilson@gmail.com>
2020-07-19 18:19:01 -0400

    working on styling for settings page

--- react-passport-mongo/client/src/pages/Settings/index.js
+++ react-passport-mongo/client/src/pages/Settings/index.js
@@ -4,7 +4,8 @@ import Toggle from '../../components/ToggleShow/toggle';
 import Adddog from '../../components/AddDogForm/addDogForm';
 import { user } from '../../utils/API';
 import { Button } from '../../components/Button';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import style from './style.css';
 
 
 
@@ -12,11 +13,11 @@ export default (props) => {
 	return (
 		<Container>
 			{console.log(props.dog)}
-			<div className='card' style={{ width: '18rem' }}>
-					<img className='card-img-top' alt='Dog Profile Image'></img>
+			<div className='card'>
+					{/* <img className='card-img-top' alt='Dog Profile Image'></img> */}
 					<div className='card-body'>
-						<h2 className='card-title'>{props.user.name}</h2>
-						<p className='card-text'>{props.user.email}</p>
+						<h2 className='card-title miniProfileFont'>{props.user.name}</h2>
+						<p className='card-text miniProfileFont'>{props.user.email}</p>
 					</div>
 					<ul className='list-group list-group-flush'>
 						<li className='list-group-item'><button>
